31f2e651c335f342dc0eb0f8c1b24d5ee2d45cda
Isaiah <zaymgmt707@gmail.com>
2024-05-25 04:12:13 -0700

    updates index.js for elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = "Isaiah";
+newHeader.textContent = `${yourName} is the champion`;
+document.body.appendChild(newHeader);
